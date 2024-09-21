e74e9c0c586f069727862fd43b497baa1d060fb7
DiegoNoLoler <di.santiagos@duocuc.cl>
2024-09-20 23:47:40 -0300

    login

--- RegistrAPP/src/app/login/login.page.ts
+++ RegistrAPP/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  user: string;
+  contra: string;
 
-  constructor() { }
+  constructor(private ToastController: ToastController) {
+    this.user = 'Usuario1';
+    this.contra = 'MiClav3';
+  }
 
   ngOnInit() {
   }
 
+  async login() {
+    if (this.user === 'Usuario1' && this.contra === 'MiClav3') {
+      const toast = await this.ToastController.create({
+        message: 'Bienvenido',
+        position: 'middle',
+        duration: 2000
+      });
+      toast.present();
+    } else {
+      const toast = await this.ToastController.create({
+        message: 'Usuario o contraseña incorrectos',
+        position: 'middle',
+        duration: 2000,
+        color: 'danger'
+      });
+      toast.present();
+    }
+  }
 }
