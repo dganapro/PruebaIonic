0118e6e57b4e71b9ed50b6ab316230cb807f523b
DiegoNoLoler <di.santiagos@duocuc.cl>
2024-09-24 11:01:48 -0300

    asd

--- RegistrAPP/src/app/mis-asignatura/mis-asignatura.page.ts
+++ RegistrAPP/src/app/mis-asignatura/mis-asignatura.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface Asignatura {
+  id: number;
+  nombre: string;
+  siglas: string;
+}
 
 @Component({
   selector: 'app-mis-asignatura',
@@ -6,10 +13,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mis-asignatura.page.scss'],
 })
 export class MisAsignaturaPage implements OnInit {
+  asignaturas: Asignatura[] = [
+    { id: 1, nombre: 'Matemáticas', siglas: 'mdi112' },
+    { id: 2, nombre: 'Ciencias', siglas: 'sci233' },
+    // Agrega más asignaturas según sea necesario
+  ];
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  verAsignatura(id: number) {
+    this.router.navigate(['/asignatura', id]);
   }
-
 }
